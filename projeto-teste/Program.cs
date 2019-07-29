8d7b95e0033c1d4a58a36ff07e63cbcc25d5715b
Raphael Tercini Costa <raphael.tercini@hotmail.com>
2019-07-29 13:14:24 -0300

    Acrescentada a mensagem de tchau!

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -13,6 +13,7 @@ namespace projeto_teste
             Console.WriteLine("Olá mundo!");            
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um Bom dia!");
+            Console.WriteLine("Tchau!");
             Console.ReadLine();
             
         }
