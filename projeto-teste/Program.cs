a84832dd198c7000c8ffec4905e4629d84e0b12b
Raphael Tercini Costa <raphael.tercini@hotmail.com>
2019-07-29 13:09:33 -0300

    Acrescentando a mensagem tenha um bom dia

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -10,9 +10,9 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá mundo!");
-            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Olá mundo!");            
             Console.WriteLine("Como você está hoje?");
+            Console.WriteLine("Tenha um Bom dia!");
             Console.ReadLine();
             
         }
