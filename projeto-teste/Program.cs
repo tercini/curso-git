556a67a644a01c516f5e5073029fa6335687ef6e
Raphael Tercini Costa <raphael.tercini@hotmail.com>
2019-07-29 13:21:18 -0300

    Acrescentando tracos

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -10,10 +10,12 @@ namespace projeto_teste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("----------------------------------------")
             Console.WriteLine("Olá mundo!");            
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um Bom dia!");
             Console.WriteLine("Tchau!");
+            Console.WriteLine("----------------------------------------")
             Console.ReadLine();
             
         }
